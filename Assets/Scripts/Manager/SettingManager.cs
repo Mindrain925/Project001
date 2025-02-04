fb4c9b0ac4046fa698bd1e8aca0f9fd88346447c
prof_abyss <yejun6210@gmail.com>
2025-02-04 23:10:50 +0900

    Feat : Implement default setting menu
    1. Add Bgm volume slider in SettingMenu
    2. Add SoundEffect volume slide in SettingMenu
    3. Add GameQuit Button int SettingMenu

--- Assets/Scripts/Manager/SettingManager.cs
+++ Assets/Scripts/Manager/SettingManager.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class SettingManager : MonoBehaviour
 {
     public static SettingManager Instance { get; private set; }
-    [SerializeField] private GameObject BgmManager;
-    [SerializeField] private GameObject SoundEffectManager;
-    [SerializeField] private GameObject SettingManagerGUI;
-    
+    [SerializeField] private AudioSource bgmAudioSource;
+    [SerializeField] private AudioSource soundEffectAudioSource;
+    [SerializeField] private Slider bgmVolumeSlider;
+    [SerializeField] private Slider soundEffectVolumeSlider;
+
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
@@ -21,11 +23,27 @@ public class SettingManager : MonoBehaviour
         // Assign the static instance and make it persist across scenes
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        bgmVolumeSlider.value = bgmAudioSource.volume * 10;
+        soundEffectVolumeSlider.value = soundEffectAudioSource.volume;
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)){
-            SettingManagerGUI.SetActive(!SettingManagerGUI.activeSelf);
+            gameObject.transform.GetChild(0).gameObject.SetActive(!gameObject.transform.GetChild(0).gameObject.activeSelf);
         }
+    }
+
+    //* bgmAudioSource.volume is 10 times smaller than bgamVolumeSlider.value because the bgm is so loud : for now
+    public void SetBgmVolume(){
+        bgmAudioSource.volume = bgmVolumeSlider.value * 0.1f;
+    }
+
+    public void SetSoundEffectVolume(){
+        soundEffectAudioSource.volume = soundEffectVolumeSlider.value;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
     }
 }
